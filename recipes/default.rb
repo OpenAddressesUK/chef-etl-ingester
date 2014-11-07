a9bd6f403536f6b9ebc81e2b2829c8c0a392e120
pikesley <github@orgraphone.org>
2014-11-07 16:36:21 +0000

    Code is deployed

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,29 @@
 package 'git'
 package 'python-mysqldb'
 
-user 'openaddresses' do
-  home '/home/openaddresses'
+oa_user = 'openaddresses'
+
+user oa_user do
+  home '/home/%s' % oa_user
   shell '/bin/bash'
   supports manage_home: true
   action :create
+end
+
+deploy_root = '/home/%s/etl' % oa_user
+
+directory deploy_root do
+  owner oa_user
+  action :create
+end
+
+%w{
+  common-ETL
+  companies-house-ETL
+}.each do |etl|
+  git '%s/%s' % [ deploy_root, etl ] do
+    user oa_user
+    repository 'https://github.com/OpenAddressesUK/%s' % etl
+    action :sync
+  end
 end
--- test/integration/default/serverspec/localhost/etl_spec.rb
+++ test/integration/default/serverspec/localhost/etl_spec.rb
@@ -11,3 +11,19 @@ end
 describe user 'openaddresses' do
   it {should exist }
 end
+
+describe file '/home/openaddresses/etl/common-ETL' do
+  it { should be_directory }
+end
+
+describe file '/home/openaddresses/etl/common-ETL/setup.py' do
+  its(:content) { should match /name='common_etl',/ }
+end
+
+describe file '/home/openaddresses/etl/companies-house-ETL' do
+  it { should be_directory }
+end
+
+describe file '/home/openaddresses/etl/companies-house-ETL/README.md' do
+  its(:content) { should match /companies-house-ETL/ }
+end
